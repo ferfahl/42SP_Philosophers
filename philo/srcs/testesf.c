238e975da988a6ac23767a1ca10b2b2999ad564e
Fernanda Alves fahl <fe.alves.fahl@gmail.com>
2023-06-01 18:50:18 -0300

    [studies] progress with threads

--- philo/srcs/testesf.c
+++ philo/srcs/testesf.c
@@ -1,66 +1,16 @@
 #include "philo.h"
-#include "philo.h"
-typedef	unsigned long	t_lu;
-pthread_mutex_t	mutex;
-int count = 0;
-t_lu	starttime;
 
-void	print_sleep(int count, int timestamp)
+void	print_test(t_init *init)
 {
-	printf("%d Philosopher %d is sleeping\n", timestamp, count);
-}
+	int	index;
 
-void	print_think(int count, int timestamp)
-{
-	printf("%d Philosopher %d is thinking\n", timestamp, count);
-}
-
-void	print_eat(int count, int timestamp)
-{
-	printf("%d Philosopher %d is eating\n", timestamp, count);
-}
-
-int	timex()
-{
-	struct timeval	time;
-
-	gettimeofday(&time, NULL);
-	return ((time.tv_sec * 1000) + (time.tv_usec / 1000));
-}
-
-void	*routine()
-{
-	t_lu	timestamp;
-	t_lu	endtime;
-
-	pthread_mutex_lock(&mutex);
-	count++;
-	endtime = timex();
-	timestamp = (endtime - starttime);
-	print_eat(count, timestamp);
-	usleep(100000);
-	endtime = timex();
-	timestamp = (endtime - starttime);
-	print_sleep(count, timestamp);
-	usleep(200000);
-	endtime = timex();
-	timestamp = (endtime - starttime);
-	print_think(count, timestamp);
-	pthread_mutex_unlock(&mutex);
-	return (NULL);
-}
-
-int	main(void)
-{
-	pthread_t	philo1, philo2, philo3;
-	pthread_mutex_init(&mutex, NULL);
-
-	starttime = timex();
-	pthread_create(&philo1, NULL, &routine, NULL);
-	pthread_create(&philo2, NULL, &routine, NULL);
-	pthread_create(&philo3, NULL, &routine, NULL);
-	pthread_join(philo1, NULL);
-	pthread_join(philo2, NULL);
-	pthread_join(philo3, NULL);
-	pthread_mutex_destroy(&mutex);
+	index = 0;
+	// (void)init;
+	// printf("Teste\n");
+	printf("number_of_philosophers: %d\n", init->nbr_of_philos);
+	printf("time_to_die: %d\n", init->time_to_die);
+	printf("time_to_eat: %d\n", init->time_eating);
+	printf("time_to_sleep: %d\n", init->time_sleeping);
+	printf("number_of_times_each_philosopher_must_eat: %d\n", init->nbr_of_times_to_eat);
+	free(init);
 }
